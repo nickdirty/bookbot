13326b8a34e41aa2b3bddff60cd5d6599258af53
nickdirty <nickdanielphd@gmail.com>
2024-01-03 23:59:51 -0700

    Finalized Project

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-with open("books/frankenstein.txt") as f:
+activefile = "books/frankenstein.txt"
+
+with open(activefile) as f:
     
     def first(n):
         return n[0]
@@ -7,36 +9,23 @@ with open("books/frankenstein.txt") as f:
         return n[1]
 
     book = f.read()
-    #title = f.name()
-    print(f"--- Begin report of x ---")
-    #lowercase = book.lower()
-    #print(book.lower())
     letdic = dict()
-    # this prints all the letters in the book on their own lines
+    print(f"--- Begin report of {activefile} ---")
+    
+    # this counts all the symbols in the book and outputs as a dict
     for i in book.lower():
         if i in letdic:
             letdic[i] += 1
         else:
             letdic[i] = 1
+
+    # translate the dictionary into a tuple, then use the second key to sort        
     thislist = letdic.items()
     sortlist = sorted(thislist, key = second, reverse = True)
+    #print(sortlist[1])
 
-    for i in sortlist:
-        if first(i).isalpha() == False
-            sortlist.pop(i)
-    
-    print(sortlist)
-        
-    
-    
-    #print(sortlist)
-    
-    
-    #dickey = list(letdic.keys())
-    #dicval = list(letdic.values())
-    #counts = []
-    
-    
-    #dickey.sort()
-    #print(dickey)
-    #print(sorted(dicval))
+    for i, k in enumerate(sortlist):
+        if first(k).isalpha() == True:
+            print(f"The '{first(k)}' character was found {second(k)} times")
+                  
+    print("--- End Report ---")
